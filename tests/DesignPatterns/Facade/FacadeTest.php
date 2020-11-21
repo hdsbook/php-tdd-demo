9ad37f149efb690ad574a520ee69a944924ea015
zang <myginnygo@gmail.com>
2020-11-21 18:41:59 +0800

    fix facade namespace problem

--- tests/DesignPatterns/Facade/FacadeTest.php
+++ tests/DesignPatterns/Facade/FacadeTest.php
@@ -1,20 +1,31 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use Src\designPatterns\Facade\Facade;
+use Src\DesignPatterns\Facade\FacadeA;
+use Src\DesignPatterns\Facade\FacadeB;
 
 /**
  * FacadeTest
  */
 class FacadeTest extends TestCase
 {
-    public function testDoSomething()
+    public function testDoSomethingA()
     {
-        $facade = new Facade();
+        $facade = new FacadeA();
         $result = $facade->doSomeThing();
         self::assertSame([
             'API 1 do something',
             'API 2 do something',
+        ], $result);
+    }
+
+    public function testDoSomethingB()
+    {
+        $facade = new FacadeB();
+        $result = $facade->doSomeThing();
+        self::assertSame([
+            'API 2 do something',
+            'API 3 do something',
         ], $result);
     }
 }
